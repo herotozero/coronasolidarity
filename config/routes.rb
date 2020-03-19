082920378d370e99e8d814be36c2d72d1e77b2f5
Mike Yousef <msy@posteo.de>
2020-03-19 20:07:33 +0100

    adding user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'about', to: 'pages#about', as: :about
+  get 'users', to: 'users#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
