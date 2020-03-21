6630ab093e1ece0a4e43e7c0cb7f1d45dfd88a0c
Jan Westerhoff <jan.westerhoff@t-online.de>
2020-03-21 14:43:20 +0100

    Index für Helfer erstellt

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -1,17 +1,24 @@
 class HelpersController < ApplicationController
   def new
-
   end
 
   def create
-
+    @helper = Helper.new(params)
+    @helper.save
+    redirect_to root_path
   end
 
   def index
+    @helpers = Helper.all
+  end
+
+  def destroy
 
   end
 
-  def show
-
+  private
+  def helpers_params
+    params.require(:helper).permit(:title, :first_name, :last_name, :email, :phone, :qualification, :specialty, :city, :commitment, :start_date, :employment_status)
   end
+
 end
