9e6b6cf39771009e546aaa24356205654b791d83
Mike Yousef <msy@posteo.de>
2020-03-21 15:41:37 +0100

    add validations to helper model

--- app/models/helper.rb
+++ app/models/helper.rb
@@ -1,2 +1,3 @@
 class Helper < ApplicationRecord
+  validates :title, :first_name, :last_name, :email, :phone, :qualification, :specialty, :city, :commitment, :availability, :start_date, :employment_status, presence: true
 end
