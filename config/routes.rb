80dd0f23fb9a789c29a738a44c5516be92452bd7
jmorrice <jonny@morrices.de>
2020-03-21 15:44:19 +0100

    adds form view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact', as: :contact
   get 'about', to: 'pages#about', as: :about
   resources :helpers, only: [:index, :new, :create, :destroy]
-  root to: 'pages#home'
+  root to: 'helpers#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
