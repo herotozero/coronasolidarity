793caf268b8a126097756f9315d6db0d8e1d8baf
Jan Westerhoff <jan.westerhoff@t-online.de>
2020-03-21 22:36:21 +0100

    Landingpage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'home', to: 'pages#home', as: :home
   resources :helpers, only: [:index, :new, :create, :destroy]
-  root to: 'helpers#new'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
